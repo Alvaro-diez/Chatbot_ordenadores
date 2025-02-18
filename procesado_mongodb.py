6e2bee8c363476e0f915e7392c3d3a6411cfef65
Alvaro-diez <alvaro.diez@tajamar365.com>
2025-02-18 10:35:38 +0100

    Commit logging de prueba

--- procesado_mongodb.py
+++ procesado_mongodb.py
@@ -32,47 +32,45 @@ collection = db[collection_name]
 
 def insertar_documento(blob):
     logging.info(f"Insertando documento {blob}")
-    if blob.endswith('.labels.json'):
-        blob_client = blob_service_client.get_blob_client(container="f-tecnicas", blob=blob)
-        blob_data = blob_client.download_blob()
-        json_data = json.loads(blob_data.readall())
-        logging.info(f"Datos extraídos de {blob}")
+    for blob in container_client.list_blobs():
+        if blob.endswith('.labels.json'):
+            blob_client = blob_service_client.get_blob_client(container="f-tecnicas", blob=blob)
+            blob_data = blob_client.download_blob()
+            json_data = json.loads(blob_data.readall())
+            logging.info(f"Datos extraídos de {blob}")
 
-        json_limpio = {
-            "document": "",
-            "labels": [],
-        }
+            json_limpio = {
+                "document": "",
+                "labels": [],
+            }
 
-        if "document" in json_data and "labels" in json_data:
-            # Formato original con "document" y "labels"
-            json_limpio["document"] = json_data["document"]
-            for label in json_data["labels"]:
-                value_text = "".join(value["text"] for value in label["value"])
-                json_limpio["labels"].append(
-                    {
-                    "label": label["label"],
-                    "value": value_text
-                    }
-                )
+            if "document" in json_data and "labels" in json_data:
+                # Formato original con "document" y "labels"
+                json_limpio["document"] = json_data["document"]
+                for label in json_data["labels"]:
+                    value_text = "".join(value["text"] for value in label["value"])
+                    json_limpio["labels"].append(
+                        {
+                        "label": label["label"],
+                        "value": value_text
+                        }
+                    )
 
-        elif "documents" in json_data and "fields" in json_data["documents"][0]:
-            json_limpio["document"] = blob.name.split(".")[0]+".pdf"
-            
-            for doc in json_data["documents"]:
-                for field_name, field_value in doc.get("fields", {}).items():
-                    value_string = field_value.get("valueString")
-                    
-                    if value_string:
-                        json_limpio["labels"].append({
-                            "label": field_name,
-                            "value": value_string
-                        })
+            elif "documents" in json_data and "fields" in json_data["documents"][0]:
+                json_limpio["document"] = blob.name.split(".")[0]+".pdf"
+                
+                for doc in json_data["documents"]:
+                    for field_name, field_value in doc.get("fields", {}).items():
+                        value_string = field_value.get("valueString")
+                        
+                        if value_string:
+                            json_limpio["labels"].append({
+                                "label": field_name,
+                                "value": value_string
+                            })
 
-        # else:
-        #     print(f"Formato desconocido en {blob.name}")
-        #     continue 
 
-        collection.insert_one(json_limpio)
+            collection.insert_one(json_limpio)
 
 
 # for blob in container_client.list_blobs():
